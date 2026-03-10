6e899b146aac10301c28ec7b35eda5f313b9c790
kalycaokta-ux <kalycaokta@gmail.com>
2026-03-10 20:35:14 +0700

    masukkan tinggi badan

--- Praktikum4.cpp
+++ Praktikum4.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 // Prosedur untuk memasukkan data
 void inputData(float &berat, float &tinggi) {
-    cout << "Masukkan Berat Badan (kg) :";
+    cout << "Masukkan Berat Badan (kg) : ";
     cin >> berat;
+    cout << "Masukkan Tinggi Badan (m) : ";
+    cin >> tinggi;
 }
